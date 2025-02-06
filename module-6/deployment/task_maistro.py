f1cd09851b313d8a622afa726102a9a5261b9b74
vikaskapur <vikaskapur04@gmail.com>
2025-02-06 00:47:35 +0000

    add deployment changes for cloud compute

--- module-6/deployment/task_maistro.py
+++ module-6/deployment/task_maistro.py
@@ -138,7 +138,7 @@ class UpdateMemory(TypedDict):
     update_type: Literal['user', 'todo', 'instructions']
 
 # Initialize the model
-model = ChatOpenAI(model="gpt-4o", temperature=0)
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 ## Create the Trustcall extractors for updating the user profile and ToDo list
 profile_extractor = create_extractor(
